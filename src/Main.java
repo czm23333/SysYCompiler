56a438c23280d1752a53188846c5a0043a12c758
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-02-25 18:58:17 +0800

    Init Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.exit(1);
-        }
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) System.exit(1);
+        System.out.print(Files.readString(Paths.get(args[0])));
     }
 }
