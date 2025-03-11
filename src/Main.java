4884e2ed6eaeed3e9a1b19482b21f5771d7a770d
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-03-11 12:21:03 +0800

    Parser rules

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,35 @@ import org.antlr.v4.runtime.*;
 import java.io.IOException;
 
 public class Main {
-    private static void printToken(Vocabulary vocabulary, Token token) {
-        if (token.getType() == SysYLexer.INTEGER_CONST)
-            System.err.printf("%s %d at Line %d.\n", vocabulary.getSymbolicName(token.getType()),
-                    Integer.decode(token.getText()), token.getLine());
-        else System.err.printf("%s %s at Line %d.\n", vocabulary.getSymbolicName(token.getType()), token.getText(),
-                token.getLine());
-    }
-
     public static void main(String[] args) throws IOException {
         if (args.length == 0) System.exit(1);
-        var stream = CharStreams.fromFileName(args[0]);
-        var lexer = new SysYLexer(stream);
-        var vocabulary = lexer.getVocabulary();
+        var parser = getSysYParser(CharStreams.fromFileName(args[0]));
+        var formatter = new SysYFormatVisitor();
+        System.out.print(formatter.visit(parser.program()));
+    }
 
-        final boolean[] hasError = {false};
+    private static SysYParser getSysYParser(CharStream stream) {
+        var lexer = new SysYLexer(stream);
         lexer.removeErrorListeners();
         lexer.addErrorListener(new BaseErrorListener() {
             @Override
             public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
                     int charPositionInLine, String msg, RecognitionException e) {
-                hasError[0] = true;
                 System.err.printf("Error type A at Line %d: %s at char %d.\n", line, msg, charPositionInLine);
+                System.exit(0);
             }
         });
-        var tokens = lexer.getAllTokens();
-        if (!hasError[0])
-            tokens.forEach(token -> printToken(vocabulary, token));
+
+        var parser = new SysYParser(new BufferedTokenStream(lexer));
+        parser.removeErrorListeners();
+        parser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
+                    int charPositionInLine, String msg, RecognitionException e) {
+                System.err.printf("Error type B at Line %d: %s at char %d.\n", line, msg, charPositionInLine);
+                System.exit(0);
+            }
+        });
+        return parser;
     }
 }
---
+++ src/SysYParser.g4
@@ -0,0 +1,48 @@
+parser grammar SysYParser;
+
+options {
+    tokenVocab = SysYLexer;
+}
+
+program : (varDef | funcDef)*;
+
+basicType : INT;
+retType : basicType | VOID;
+
+constPrefix : CONST?;
+incompleteArray : L_BRACKT R_BRACKT;
+arrayPostfixSingle : L_BRACKT length=expr R_BRACKT;
+arrayPostfix : arrayPostfixSingle*;
+eqInitializeVal : expr # single
+                | L_BRACE (eqInitializeVal (COMMA eqInitializeVal)*)? R_BRACE # array;
+initializer : ASSIGN value=eqInitializeVal # eqInitializer;
+varDefEntry : name=IDENT arrayPostfix init=initializer?;
+varDef : constPrefix type=basicType varDefEntry (COMMA varDefEntry)* SEMICOLON;
+
+funcParam : type=basicType name=IDENT incompleteArray? arrayPostfix;
+funcDef : r_type=retType name=IDENT L_PAREN (funcParam (COMMA funcParam)*)? R_PAREN stmtBlock;
+
+leftVal : IDENT arrayPostfix;
+funcRealParam : expr;
+expr : value=INTEGER_CONST # const
+     | func=IDENT L_PAREN (funcRealParam (COMMA funcRealParam)*)? R_PAREN # functionCall
+     | leftVal # access
+     | L_PAREN expr R_PAREN # paren
+     | op=(PLUS | MINUS | NOT) x=expr # unary
+     | l=expr op=(MUL | DIV | MOD) r=expr # muls
+     | l=expr op=(PLUS | MINUS) r=expr # adds
+     | l=expr op=(LT | GT | LE | GE) r=expr # rels
+     | l=expr op=(EQ | NEQ) r=expr #eqs
+     | l=expr AND r=expr # and
+     | l=expr OR r=expr # or;
+
+stmtBlock : L_BRACE stmt* R_BRACE;
+stmt : expr? SEMICOLON # expression
+     | varDef # vdef
+     | lvalue=leftVal ASSIGN value=expr SEMICOLON # assignment
+     | stmtBlock # block
+     | IF L_PAREN cond=expr R_PAREN stmtTrue=stmt (ELSE stmtFalse=stmt)? # if
+     | WHILE L_PAREN cond=expr R_PAREN stmtTrue=stmt # while
+     | BREAK SEMICOLON # break
+     | CONTINUE SEMICOLON # continue
+     | RETURN ret=expr? SEMICOLON # return;
